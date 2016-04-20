5dc553bfa33cf400f24539e56ac6bd269eb7c1ff
Gaurav <gauravshetty4@gmail.com>
2016-04-20 18:14:00 +0530

    Bugs fixed

--- BloodPressure/app/src/main/java/abhi/kurla/bloodpressure/MainActivity.java
+++ BloodPressure/app/src/main/java/abhi/kurla/bloodpressure/MainActivity.java
@@ -2,23 +2,29 @@ package abhi.kurla.bloodpressure;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView systolicView, diastolicView, heartRateView;
-    String systolic, diastolic, heartRate = "000";
+    String systolic = "000";
+    String diastolic = "000";
+    String heartRate = "000";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setTitle("BP Monitor");
         Bundle extras = getIntent().getExtras();
         if(extras != null) {
             systolic = extras.getString("systolic");
             diastolic = extras.getString("diastolic");
             heartRate = extras.getString("heart_rate");
-        } else {
+        } else if(savedInstanceState !=null){
             systolic = (String) savedInstanceState.getSerializable("systolic");
             diastolic = (String) savedInstanceState.getSerializable("diastolic");
             heartRate = (String) savedInstanceState.getSerializable("heart_rate");
